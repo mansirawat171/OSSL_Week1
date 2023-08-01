922909a6c89b9f2ba07dd0d31e8f787c94a834e3
mansirawat171 <141109599+mansirawat171@users.noreply.github.com>
2023-08-01 10:29:36 +0530

    Create Multiplication.cpp

---
+++ Multiplication.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int multiply(int &a, int &b){
+    int ans = a*b;
+    return ans;
+}
+
+int main () {
+    int a,b;
+    string name;
+    cout<<"Enter name: "<<endl;
+    cout<<"--------------HELLO"<<name<<"---------------"<<endl;
+    cout<<"Enter a: "<<endl;
+    cin>>a;
+    cout<<"Enter b: "<<endl;
+    cin>>b;
+    cout<<"--------------ANSWER--------------- "<<endl;
+    cout<<multiply(a,b)<<endl;
+    cout<<"------------THANK YOU------------- "<<endl;
+
+    return 0;
+}
+
